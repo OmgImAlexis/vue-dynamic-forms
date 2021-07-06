ce73cec7f09019bdb8d8bfae5e6ced6d2ac24103
Alexis Tyler <xo@wvvw.me>
2021-07-06 13:08:21 +0930

    fix(types): use ValidatorFn for builtin validators

--- src/core/utils/validators.ts
+++ src/core/utils/validators.ts
@@ -6,11 +6,11 @@ export const isEmptyInputValue = (value: ControlValue): boolean =>
 const EMAIL_REGEXP = /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
 const URL_REGEXP = /^((?:(https?):\/\/)?((?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]|[0-9])\.(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]|[0-9])\.)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]|[0-9])\.)(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9][0-9]|[0-9]))|(?:(?:(?:\w+\.){1,2}[\w]{2,3})))(?::(\d+))?((?:\/[\w]+)*)(?:\/|(\/[\w]+\.[\w]{3,4})|(\?(?:([\w]+=[\w]+)&)*([\w]+=[\w]+))?|\?(?:(wsdl|wadl))))$/;
 
-export const required = (value: ControlValue): ValidationErrors => ({
+export const required: ValidatorFn = (value: ControlValue): ValidationErrors => ({
   required: isEmptyInputValue(value) ? true : null,
 });
 
-export const min = (min: number) => (value: ControlValue): ValidationErrors => {
+export const min = (min: number): ValidatorFn => (value: ControlValue): ValidationErrors => {
   if (isEmptyInputValue(value) || isEmptyInputValue(min)) {
     return { min: null }; // don't validate empty values to allow optional controls
   }
@@ -20,7 +20,7 @@ export const min = (min: number) => (value: ControlValue): ValidationErrors => {
   };
 };
 
-export const max = (max: number) => (value: ControlValue): ValidationErrors => {
+export const max = (max: number): ValidatorFn => (value: ControlValue): ValidationErrors => {
   if (isEmptyInputValue(value) || isEmptyInputValue(max)) {
     return { max: null }; // don't validate empty values to allow optional controls
   }
@@ -32,21 +32,21 @@ export const max = (max: number) => (value: ControlValue): ValidationErrors => {
   };
 };
 
-export const email = (value: ControlValue): ValidationErrors => {
+export const email: ValidatorFn = (value: ControlValue): ValidationErrors => {
   if (isEmptyInputValue(value)) {
     return { email: null }; // don't validate empty values to allow optional controls
   }
   return { email: EMAIL_REGEXP.test(`${value}`) ? null : true };
 };
 
-export const url = (value: ControlValue): ValidationErrors => {
+export const url: ValidatorFn = (value: ControlValue): ValidationErrors => {
   if (isEmptyInputValue(value)) {
     return { url: null }; // don't validate empty values to allow optional controls
   }
   return { url: URL_REGEXP.test(`${value}`) ? null : true };
 };
 
-export const minLength = (minLength: number) => (
+export const minLength = (minLength: number): ValidatorFn => (
   value: ControlValue,
 ): ValidationErrors => {
   if (isEmptyInputValue(value)) {
@@ -62,7 +62,7 @@ export const minLength = (minLength: number) => (
   };
 };
 
-export const maxLength = (maxLength: number) => (
+export const maxLength = (maxLength: number): ValidatorFn => (
   value: ControlValue,
 ): ValidationErrors => {
   if (isEmptyInputValue(value)) {
